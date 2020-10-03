414f622411204bb120038d2a40d53ce5ebd0ca05
Daniel <daniel.olicruz13@gmail.com>
2020-10-03 13:06:08 -0300

    Adicionado informacoes JWT claims

--- src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
@@ -9,10 +9,12 @@ import java.util.Collections;
 @Getter
 public class AuthUser extends User {
 
-    private String fullName;
+    private final Long userId;
+    private final String fullName;
 
     public AuthUser(Usuario usuario) {
         super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
         this.fullName = usuario.getNome();
+        this.userId = usuario.getId();
     }
 }
